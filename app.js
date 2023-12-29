e750faa864b009183c659d1a06f9c0ee02aa6b16
Riddhesh1009 <riddheshmalirm@gmail.com>
2023-12-29 07:15:53 +0530

    Added new file

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// const n = 12;
-// if (n > 10) {
-//   console.log("number is greater than 10");
-// } else {
-//   console.log("number is smaller than 10");
-// }
-// console.log("first node js code!!!!");
+const n = 12;
+if (n > 10) {
+  console.log("number is greater than 10");
+} else {
+  console.log("number is smaller than 10");
+}
+console.log("first node js code!!!!");
 
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(process)
+console.log(__dirname);
+console.log(__filename);
+console.log(process)
 
-// setInterval(()=>{
-//     console.log("Hello World")
-// },1000)
+setInterval(()=>{
+    console.log("Hello World")
+},1000)
 
